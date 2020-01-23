049882e3a14088ca0c7299270765929a33c3e676
Rowena Chen <rwnchen@gmail.com>
2020-01-23 16:16:04 -0500

    init commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>hallo worl</Text>
+      <Text>is this a bad idea</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
