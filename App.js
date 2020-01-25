04fedf0b4d16b0ab29b747bb1a05ed6edb68e8e5
Rowena Chen <rwnchen@gmail.com>
2020-01-25 16:36:56 -0500

    added bg

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import * as assets from './assets';
 
 import Store from './components/store';
 import { Controller } from './components/index';
@@ -8,17 +9,34 @@ export default function App() {
   return (
     <Store.Container>
       <View style={styles.container}>
-        <Controller></Controller>
+        <Background img={assets.GROUND} />
+        <Background img={assets.GRASS} />
+
+        <Controller />
       </View>
     </Store.Container>
   );
 }
 
+const Background = (props) => {
+  return <Image source={props.img} style={styles.tileBG}></Image>;
+};
+
 const styles = StyleSheet.create({
   container: {
+    position: 'relative',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tileBG: {
+    position: 'absolute',
+    top: 12,
+    left: 12,
+    width: '120%',
+    height: '120%',
+    resizeMode: 'repeat',
+    transform: [{ scale: 2 }],
   },
 });
